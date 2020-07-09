caca514535c59028fc681c14558f122302e233e1
craneox <Lawrence_Cardenas@live.com>
2020-07-09 01:05:29 -0500

    peleas estatales

---
+++ controllers/soap_clients/cliente_peleas_estatales_leer.php
@@ -0,0 +1,112 @@
+<?php
+require_once('lib/nusoap.php');
+// Check existence of id parameter before processing further
+if(isset($_GET["id"]) && !empty(trim($_GET["id"])))
+{
+    $id =  trim($_GET["id"]);
+    $cliente = new nusoap_client("http://localhost/BOMV/ws_soap/ws_peleas_estatales.php");
+	$datos = array('id' => $id);
+
+	$resultado = $cliente->call('buscarPeleaEstatal', $datos);
+	
+	$err = $cliente->getError();
+	if($err){
+		echo '<h2>Error del constructor</h2><pre>'.$err.'</pre>';
+		echo '<h2>Request</h2><pre>'.htmlspecialchars($cliente->request, ENT_QUOTES).'</pre>';
+		echo '<h2>Response</h2><pre>'.htmlspecialchars($cliente->response, ENT_QUOTES).'</pre>';
+		echo '<h2>Debug</h2><pre>'.htmlspecialchars($cliente->getDebug(), ENT_QUOTES).'</pre>';
+	}else{
+        $array = explode("<", $resultado);
+        $id = $array[0];
+		$categoria = $array[1];
+        $id_juez1 = $array[2];
+        $id_juez2 = $array[3];
+        $id_juez3 = $array[4];
+        $id_juez4 = $array[5];
+        $id_boxeador1 = $array[6];
+        $id_boxeador2 = $array[7];
+		$fecha = $array[8];
+        $hora = $array[9];
+	}
+} 
+else
+{
+    // URL doesn't contain id parameter. Redirect to error page
+    header("location: error.php");
+    exit();
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>View Record</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <style type="text/css">
+        .wrapper{
+            width: 500px;
+            margin: 0 auto;
+        }
+    </style>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="page-header">
+                        <h1>Ver Pelea Estatal</h1>
+                    </div>
+                    <!--id, categoria, id_juez1, id_juez2, id_juez3, id_juez4, id_boxeador1, id_boxeador2, fecha, hora-->
+                    <div class="form-group">
+                        <label>Id Pelea Estatal</label>
+                        <p class="form-control-static"><?php echo $id; ?></p>
+                    </div>
+                    <div class="form-group">
+                        <label>Categoria</label>
+                        <p class="form-control-static"><?php echo $categoria; ?></p>
+                    </div>
+                    <div class="form-group">
+                        <label>Id juez1</label>
+                        <p class="form-control-static"><?php echo $id_juez1; ?></p>
+                    </div>
+                    <div class="form-group">
+                        <label>Id juez2</label>
+                        <p class="form-control-static"><?php echo $id_juez2; ?></p>
+                    </div>
+                    <div class="form-group">
+                        <label>Id juez3</label>
+                        <p class="form-control-static"><?php echo $id_juez3; ?></p>
+                    </div>
+                    <div class="form-group">
+                        <label>Id juez4</label>
+                        <p class="form-control-static"><?php echo $id_juez4; ?></p>
+                    </div>
+                    <div class="form-group">
+                        <label>Id boxeador1</label>
+                        <p class="form-control-static"><?php echo $id_boxeador1; ?></p>
+                    </div>
+                    <div class="form-group">
+                        <label>Id boxeador2</label>
+                        <p class="form-control-static"><?php echo $id_boxeador2; ?></p>
+                    </div>
+                    <div class="form-group">
+                        <label>Fecha</label>
+                        <p class="form-control-static"><?php echo $fecha; ?></p>
+                    </div>
+                    <div class="form-group">
+                        <label>Hora</label>
+                        <p class="form-control-static"><?php echo $hora; ?></p>
+                    </div>
+                    <p><a href="../../views/peleas_estatales.php" class="btn btn-primary">Regresar</a></p>
+                </div>
+            </div>        
+        </div>
+    </div>
+</body>
+</html>
---
+++ views/peleas_estatales.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Peleas Estatales</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <style type="text/css">
+        body{
+            background-image: url('../resources/ali.jpg');
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+            background-size: cover;
+        }
+        .opacity
+        {
+            opacity:0.8; /* Opacidad 60% */
+        }
+    </style>
+    <script type="text/javascript">
+        $(document).ready(function(){
+            $('[data-toggle="tooltip"]').tooltip();   
+        });
+    </script>
+</head>
+<body>
+<nav class="navbar sticky-top navbar-expand-lg navbar-light bg-light" style="background-image: linear-gradient(to right, white, cyan);">
+  <a class="navbar-brand" href="#">BOMV</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="noticias.php">Noticias<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Gimnasios</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="boxeadores.php">Boxeadores</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Jueces</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Tablas de peleas</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Peleas municipales</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Resultados de peleas municipales</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Peleas estatales</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Resultados de peleas estatales</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+    <div class="wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="page-header clearfix">
+                        <h2 class="pull-left" style="color:white;">Peleas Estatales</h2>
+                        <a href="../controllers/soap_clients/cliente_peleas_estatales_agregar.php" class="btn btn-success pull-right">Agregar nueva pelea estatal</a>
+                    </div>
+                    <?php
+                        require_once('../ws_soap/lib/nusoap.php');
+                        $cliente = new nusoap_client("http://localhost/BOMV/ws_soap/ws_peleas_estatales.php");
+                        $datos = array();
+                    
+                        $resultado = $cliente->call('mostrarPeleaEstatal', $datos);
+                        
+                        $err = $cliente->getError();
+                        if($err){
+                            echo '<h2>Error del constructor</h2><pre>'.$err.'</pre>';
+                            echo '<h2>Request</h2><pre>'.htmlspecialchars($cliente->request, ENT_QUOTES).'</pre>';
+                            echo '<h2>Response</h2><pre>'.htmlspecialchars($cliente->response, ENT_QUOTES).'</pre>';
+                            echo '<h2>Debug</h2><pre>'.htmlspecialchars($cliente->getDebug(), ENT_QUOTES).'</pre>';
+                        }
+                        else
+                        {
+                            echo $resultado;
+                        }
+                    ?>
+                </div>
+            </div>        
+        </div>
+    </div>
+</body>
+</html>
