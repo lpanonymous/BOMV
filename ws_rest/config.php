0803db5fa7b468e5433d6cc60825cff5458cbb35
lpanonymous <lpanonymous0101@gmail.com>
2020-07-18 14:38:29 -0500

    inicio usuarios "noticias" agregado, falta mostrar un modal al hacer clic en boton mas información

---
+++ ws_rest/config.php
@@ -0,0 +1,10 @@
+<?php
+/* Código tomado de: https://www.codigonaranja.com/2018/crear-restful-web-service-php */
+
+$db = [
+	'host' => 'localhost',
+	'username' => 'root',
+    'password' => '',
+    'db' => 'torneo_box_olimpico' //Nombre de base de datos
+];
+?>
---
+++ ws_rest/utils.php
@@ -0,0 +1,34 @@
+<?php
+/* Código tomado de: https://www.codigonaranja.com/2018/crear-restful-web-service-php */
+
+//Abrir conexion a la base de datos
+function connect($db){
+	try {
+		$conn = new PDO("mysql:host={$db['host']};dbname={$db['db']};charset=utf8", $db['username'], $db['password']);
+
+		// set the PDO error mode to exception
+		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		return $conn;
+	} catch (PDOException $exception) {
+		exit($exception->getMessage());
+	}
+}
+
+
+//Obtener parametros para updates
+function getParams($input){
+	$filterParams = [];
+	foreach($input as $param => $value)	{
+		$filterParams[] = "$param=:$param";
+	}
+	return implode(", ", $filterParams);
+}
+
+//Asociar todos los parametros a un sql
+function bindAllValues($statement, $params){
+	foreach($params as $param => $value)	{
+		$statement->bindValue(':'.$param, $value);
+	}
+	return $statement;
+}
+?>
