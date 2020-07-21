73881e33735a17ea3870e6fca7e0350e42f56887
Leonardo <leonardogalicia2412@gmail.com>
2020-07-20 22:48:29 -0500

    Tabla responsiba
    En boxeadores, gimnasios, jueces y noticias se le aplico css tipo card view

--- views/boxeadores.php
+++ views/boxeadores.php
@@ -27,6 +27,88 @@
           height:17%;
           width:100%;
         }
+		@media
+	  only screen 
+    and (max-width: 800px), (min-device-width: 868px) 
+    and (max-device-width: 1024px)  {
+
+		/* Force table to not be like tables anymore */
+		table, thead, tbody, th, td, tr {
+			display: block;
+		}
+
+		/* Hide table headers (but not display: none;, for accessibility) */
+		thead tr {
+			position: absolute;
+			top: -9999px;
+			left: -9999px;
+		}
+
+    tr {
+      margin: 0 0 2rem 0;
+    }
+      
+    tr:nth-child(odd) {
+      background: #ccc;
+    }
+    
+		td {
+			/* Behave  like a "row" */
+			border: none;
+			border-bottom: 1px solid #eee;
+			position: relative;
+			padding-left: 50%;
+		}
+
+		td:before {
+			/* Now like a table header */
+			position: absolute;
+			/* Top/left values mimic padding */
+			top: 0;
+			left: 6px;
+			width: 45%;
+			padding-right: 100px;
+			white-space: nowrap;
+		}
+		tr td:first-child {
+           background: #5499C7;
+           font-weight:bold;
+           font-size:1.3em;
+       }
+		tbody td {
+           display: block;
+           text-align:center;
+       }
+       tbody td:before {
+           content: attr(data-th);
+           display: block;
+           text-align:center;
+       }
+		/*
+		Label the data
+    You could also use a data-* attribute and content for this. That way "bloats" the HTML, this way means you need to keep HTML and CSS in sync. Lea Verou has a clever way to handle with text-shadow.
+		*/
+		td:nth-of-type(1):before { content: "Id"; }
+		td:nth-of-type(2):before { content: "Foto:"; }
+		td:nth-of-type(3):before { content: "Gimnasio:"; }
+		td:nth-of-type(4):before { content: "Alias:"; }
+		td:nth-of-type(5):before { content: "Nombre:"; }
+		td:nth-of-type(6):before { content: "#Total Peleas:"; }
+		td:nth-of-type(7):before { content: "Peleas Ganadas:"; }
+		td:nth-of-type(8):before { content: "Pelas por K.O.:"; }
+		td:nth-of-type(9):before { content: "Peleas Perdidas:"; }
+		td:nth-of-type(10):before { content: "Perdidas por K.O.:"; }
+		td:nth-of-type(11):before { content: "Empates:"; }
+		td:nth-of-type(12):before { content: "Categoria:"; }
+		td:nth-of-type(13):before { content: "División:"; }
+		td:nth-of-type(14):before { content: "Peso:"; }
+		td:nth-of-type(15):before { content: "Altura:"; }
+		td:nth-of-type(16):before { content: "Estado:"; }
+		td:nth-of-type(17):before { content: "Ciudad:"; }
+		td:nth-of-type(18):before { content: "Munici:"; }
+				td:nth-of-type(19):before { content: "Funciones:"; }
+
+	}
     </style>
     <script type="text/javascript">
         $(document).ready(function(){
