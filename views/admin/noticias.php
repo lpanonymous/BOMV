82e9e884f16573ed2b2b37651e15300bf180f735
Leonardo <leonardogalicia2412@gmail.com>
2020-07-24 21:39:18 -0500

    actualizacion de la navegación x2

--- views/admin/noticias.php
+++ views/admin/noticias.php
@@ -100,7 +100,7 @@
     </script>
 </head>
 <body>
-<nav class="navbar sticky-top navbar-expand-lg navbar-light bg-light" style="background-image: linear-gradient(to right, white, cyan);">
+<nav class="navbar sticky-top navbar-expand-lg navbar-light navbar-fixed-top" style="background-image: linear-gradient(to right, white, cyan);">
   <a class="navbar-brand" href="#">BOMV</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
@@ -120,24 +120,25 @@
       <li class="nav-item">
         <a class="nav-link" href="jueces.php">Jueces</a>
       </li>
-
       <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <a class="nav-link dropdown-toggle" href="peleas_municipales" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           Peleas municipales
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="tablas_peleas_municipales.php">Tablas peleas municipales</a>
-          <a class="dropdown-item" href="posiciones_generales.php">Posiciones generales municipales</a>
+          <a class="dropdown-item" href="peleas_municipales.php">Peleas municipales</a>
+          <a class="dropdown-item" href="tablas_peleas_municipales.php">Tablas de peleas municipales</a>
+          <a class="dropdown-item" href="posiciones_generales.php">Posiciones municipales</a>
         </div>
       </li>
 
       <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <a class="nav-link dropdown-toggle" href="peleas_estatales" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           Peleas estatales
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="peleas_estatales.php">Peleas estatales</a>
           <a class="dropdown-item" href="tablas_peleas_estatales.php">Tablas peleas estatales</a>
-          <a class="dropdown-item" href="posiciones_estatales.php">Posiciones generales estatales</a>
+          <a class="dropdown-item" href="posiciones_estatales.php">Posiciones estatales</a>
         </div>
       </li>
     </ul>
